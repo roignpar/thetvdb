940bdbc7c253df56aa32d6c93019e7b5471bc08b
Robert Ignat <robert.ignat91@gmail.com>
2019-11-27 18:08:31 +0200

    Add error documentation

--- src/error.rs
+++ src/error.rs
@@ -1,58 +1,94 @@
+#![deny(missing_docs, missing_debug_implementations, unsafe_code)]
+
+//! Possible errors that can occur while using this crate and error helpers.
+
+use std::io::Error as IOError;
+
+use chrono::format::ParseError as TimeParseError;
 use failure::Fail;
 use jsonwebtoken::errors::Error as JWTError;
 use reqwest::Error as ReqwestError;
-use serde_json::error::Error as JsonError;
-use url::ParseError as UrlParseError;
+use serde_json::error::Error as JSONError;
+use url::ParseError as URLParseError;
 
+/// `Result` with error case set to `thetvdb::error::Error`.
 pub type Result<T> = std::result::Result<T, Error>;
 
+/// Error type containing possible failure cases
+/// of this crate.
 #[derive(Debug, Fail)]
 pub enum Error {
+    /// Occurs when parsing JSON data fails.
     #[fail(display = "JSON error: {}", _0)]
-    JSON(#[cause] JsonError),
+    JSON(#[cause] JSONError),
 
+    /// Occurs when [`reqwest`](https://docs.rs/reqwest/latest/reqwest/index.html),
+    /// the HTTP client underlying this crate, returns an error.
     #[fail(display = "HTTP error: {}", _0)]
     HTTP(#[cause] ReqwestError),
 
+    /// IO error from `std`.
     #[fail(display = "IO error: {}", _0)]
-    IO(#[cause] std::io::Error),
+    IO(#[cause] IOError),
 
+    /// Occurs when the provided API key is not valid.
     #[fail(display = "Invalid API key")]
     InvalidAPIKey,
 
+    /// Occurs when TheTVDB API returns a `5XX` error response.
     #[fail(display = "API Server error")]
     ServerError,
 
+    /// Occurs when resources (series, episodes, etc...) are
+    /// not found.
     #[fail(display = "Not found")]
     NotFound,
 
+    /// Occurs when a header returned by the API is not representable
+    /// as a string.
+    ///
+    /// See
+    /// [`reqwest::header::ToStrError`](https://docs.rs/reqwest/latest/reqwest/header/struct.ToStrError.html)
+    /// for more info.
     #[fail(display = "Non-parsable HTTP header: {}", _0)]
     InvalidHTTPHeader(#[cause] reqwest::header::ToStrError),
 
+    /// Occurs when the API doesn't return a header containing
+    /// the date and time when the series was last modified.
     #[fail(display = "Last modified data missing")]
     MissingLastModified,
 
+    /// Occurs when the API returns dates and times in formats
+    /// that are not known by this crate.
     #[fail(display = "Invalid date format: {}", _0)]
-    InvalidDateFormat(#[cause] chrono::format::ParseError),
+    InvalidDateFormat(#[cause] TimeParseError),
 
+    /// Occurs when [`Client.series_filter`](../client/struct.Client.html#method.series_filter)
+    /// is called with empty `SeriesFilterKeys`.
     #[fail(display = "No series filter keys provided")]
     MissingSeriesFilterKeys,
 
+    /// Occurs when a image URL method is called, but the
+    /// image file path is not known.
     #[fail(display = "Image data is missing")]
     MissingImage,
 
+    /// Occurs when a series website URL method is called,
+    /// but the slug is not known.
     #[fail(display = "Series slug is missing")]
     MissingSeriesSlug,
 
+    /// Occurs when a URL cannot be parsed.
     #[fail(display = "Invalid url: {}", _0)]
-    InvalidUrl(#[cause] UrlParseError),
+    InvalidUrl(#[cause] URLParseError),
 
+    /// Occurs when the JWT returned by the API on login is invalid.
     #[fail(display = "Could not decode authentication JWT: {}", _0)]
     InvalidJWT(#[cause] JWTError),
 }
 
-impl From<JsonError> for Error {
-    fn from(e: JsonError) -> Self {
+impl From<JSONError> for Error {
+    fn from(e: JSONError) -> Self {
         Self::JSON(e)
     }
 }
@@ -63,8 +99,8 @@ impl From<ReqwestError> for Error {
     }
 }
 
-impl From<std::io::Error> for Error {
-    fn from(e: std::io::Error) -> Self {
+impl From<IOError> for Error {
+    fn from(e: IOError) -> Self {
         Self::IO(e)
     }
 }
@@ -75,14 +111,14 @@ impl From<reqwest::header::ToStrError> for Error {
     }
 }
 
-impl From<chrono::format::ParseError> for Error {
-    fn from(e: chrono::format::ParseError) -> Self {
+impl From<TimeParseError> for Error {
+    fn from(e: TimeParseError) -> Self {
         Self::InvalidDateFormat(e)
     }
 }
 
-impl From<UrlParseError> for Error {
-    fn from(e: UrlParseError) -> Self {
+impl From<URLParseError> for Error {
+    fn from(e: URLParseError) -> Self {
         Self::InvalidUrl(e)
     }
 }
