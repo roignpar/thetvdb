8d35f7578c237ceeff6f49eeb8992cf92c6721c9
Robert Ignat <robert.ignat91@gmail.com>
2019-11-27 21:00:38 +0200

    Add main crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,43 @@
+#![deny(missing_docs)]
+
+//! This crate provides an async [client](./client/struct.Client.html)
+//! as well as helpful types to request and interact with data from
+//! **TheTVDB API V3**.
+//!
+//! # Examples
+//! Search for a series by name:
+//! ```no_run
+//! # use thetvdb::error::Result;
+//! use thetvdb::{Client, SearchBy};
+//!
+//! # #[tokio::main]
+//! # async fn main() -> Result<()> {
+//! let client = Client::new("YOUR_API_KEY").await?;
+//!
+//! let results = client.search(SearchBy::Name("Planet Earth")).await?;
+//!
+//! println!("{:#?}", results);
+//! # Ok(()) }
+//! ```
+//! Get a series by ID:
+//! ```no_run
+//! # use thetvdb::error::Result;
+//! # use thetvdb::Client;
+//! #
+//! # #[tokio::main]
+//! # async fn main() -> Result<()> {
+//! # let client = Client::new("KEY").await?;
+//! let series = client.series(318408).await?;
+//!
+//! assert_eq!(
+//!     series.series_name,
+//!     Some("Planet Earth II".to_string())
+//! );
+//! # Ok(()) }
+//! ```
+//!
+//! For more examples check [Client](./client/struct.Client.html).
+
 mod deserialize;
 mod serialize;
 mod urls;
